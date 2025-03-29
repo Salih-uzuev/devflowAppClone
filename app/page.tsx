3c5f86dba0140c9bd996ddf54c67016d1f1e18d3
Salih-uzuev <uzuevsalah@gmail.com>
2025-03-29 10:08:26 +0300

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 
 
 const Home =()=> (
-   <h1 className="h1-bold background-light850_dark100" >Welcome to the world of next.js 15 !</h1>
+    <>
+      <h1 className="h1-bold background-light850_dark100" >Welcome to the world of next.js 15 !</h1>
+      <h1 className="h1-bold font-space-grotesk" >Welcome to the world of next.js 15 !</h1>
+
+
+    </>
 
   );
 
