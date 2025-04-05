018085d43e2cac28f6dfa13d9f8d09dac747d2b3
Salih-uzuev <uzuevsalah@gmail.com>
2025-04-05 19:25:12 +0300

    Implement Tag Input

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,20 +5,20 @@ import RightSideBar from "@/components/navigation/RightSideBar";
 
 const RootLayout = ({children}:{children:React.ReactNode}) => {
     return (
-        <main className="background-light850_dark100 relative">
-            <Navbar/>
+        <main className="background-light850_dark100 realtive">
+            <Navbar />
 
             <div className="flex">
-                <LeftSideBar/>
+                <LeftSideBar />
+
                 <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
                     <div className="mx-auto w-full max-w-5xl">{children}</div>
                 </section>
 
-                <RightSideBar/>
+                <RightSideBar />
             </div>
-
-
         </main>
-    )
-}
+    );
+};
+
 export default RootLayout
--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -3,27 +3,64 @@ import Link from "next/link";
 import ROUTES from "@/constans/routes";
 import {Badge} from "@/components/ui/badge";
 import {getDeviconClassName} from "@/lib/utils";
+import Image from "next/image";
+import {tag} from "postcss-selector-parser";
 
 interface Props{
     _id: string,
     name: string,
     questions?: number,
     showCount?: boolean,
-    compact?: boolean
+    compact?: boolean,
+    remove?: boolean,
+    isButton?:boolean,
+    handleRemove?:()=>void;
 }
 
-const TagCard = ({_id, name, questions, showCount, compact}: Props) => {
+const TagCard = ({_id, name, questions, showCount, compact,remove, isButton, handleRemove}: Props) => {
     const iconClass = getDeviconClassName(name)
-    return <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
-        <Badge className="subtle-medium background-light800_dark300 rounded-md border-none px-4 py-2 uppercase text-light400_light500">
-            <div className="flex-center space-x-2">
-                <i className={`${iconClass} text-sm`}></i>
-                <span>{name}</span>
-            </div>
-        </Badge>
-        {showCount && <p className="small-medium text-dark500_light700">{questions}</p>}
+    const handleClick = (e:React.MouseEvent) => {
+        e.preventDefault();
 
-    </Link>
+    }
+    const Content = (
+        <>
+            <Badge className="subtle-medium background-light800_dark300 rounded-md border-none px-4 py-2 uppercase text-light400_light500 flex flex-row gap-2">
+                <div className="flex-center space-x-2">
+                    <i className={`${iconClass} text-sm`}></i>
+                    <span>{name}</span>
+                </div>
+
+                {remove &&(
+                    <Image
+                        src="/icons/close.svg"
+                        width={12}
+                        height={12}
+                        alt="close"
+                        className="dark:invert cursor-pointer object-contain invert-0"
+                        onClick={handleRemove}
+
+                    />
+                )}
+            </Badge>
+            {showCount && <p className="small-medium text-dark500_light700">{questions}</p>}
+        </>
+    )
+
+    if(compact){
+       return isButton ? (
+           <button onClick={handleClick}  className="flex justify-between gap-2">
+               {Content}
+           </button>
+       ):(
+           <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+               {Content}
+
+           </Link>
+       )
+    }
+
+
 
 }
 export default TagCard
